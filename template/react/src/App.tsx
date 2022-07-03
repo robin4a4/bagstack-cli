b7cc74b9098ecea653aa9986f7da0b2c46a2ab4d
robin marillia <robin4a4@gmail.com>
2022-07-03 22:14:39 +0200

    fixed react template

---
+++ template/react/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="header">👋 Bagstack React</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
