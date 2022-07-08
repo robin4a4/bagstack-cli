4585e1249b63938ad0a6a4650d5abdb7224440c1
robin marillia <robin4a4@gmail.com>
2022-07-09 00:25:07 +0200

    fix: clean spacing

--- src/helpers/addCommonConfigs.ts
+++ src/helpers/addCommonConfigs.ts
@@ -4,7 +4,6 @@ import { PKG_ROOT } from "../consts.js";
 
 export const addTailwindcssConfig = async (projectDir: string) => {
   const indexFileDir = path.join(PKG_ROOT, "template/addons/tailwindcss");
-
   const indexSrc = path.join(indexFileDir, "tailwind.config.js");
   const indexDest = path.join(projectDir, "/tailwind.config.js");
   await fs.copy(indexSrc, indexDest);
